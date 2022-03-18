34f7ac92912338b7384d9c7827d034fc6a7e3e94
hx <1153781489@qq.com>
2022-03-18 16:15:41 +0800

    master v 1.0.1

---
+++ src/main/java/com/athx/computermall/test.java
@@ -0,0 +1,14 @@
+package com.athx.computermall;
+
+/**
+ * @author ：Hexin
+ * @date ：Created in 2022/3/18 16:13
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hhh");
+    }
+}
